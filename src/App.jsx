5d62f1a53a0fe355112aceceff57e90076e7ca8c
AlbertSayos <asayos@fi.uba.ar>
2024-07-18 20:38:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import axios from 'axios';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+//import Filtrador from './components/filtrador';
+//import Usuario from './components/usuario';
+//import Pagar from './components/pagar';  // Importar el componente Pagar
+
+function App() {
+  const [data , setData]  = useState([])
+
+  useEffect ( ( )=>{ 
+    axios.get("https://masacr3bot.pythonanywhere.com/usuarios").then(response => {
+
+      console.log(response.data.usuarios);
+      setData([...response.data.usuarios])
+    })
+  },[]
+  ) 
+
+  return (
+    <>
+    <h1>Hola</h1>
+    <ul>
+        {data && 
+          data.map((item,i) =>
+            <li key={i} className='lora'>
+              ❤️ {item.nombre} 
+            </li>
+          )
+        }
+    </ul>
+    </>
+  );
+}
+
+export default App;
